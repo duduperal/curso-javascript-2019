0d4efb9a40a4a2f69edad2a92ae5d46916eb1f99
José Eduardo Goulart Peral <109534745+duduperal@users.noreply.github.com>
2022-12-05 13:39:15 -0300

    aula-11 ex 008, 009, 010

---
+++ aula-11/ex008-p.js
@@ -0,0 +1,5 @@
+var idade = 18
+console.log(`A sua idade é de ${idade} anos`)
+if(idade >= 18){
+    console.log(`Você é maior de idade!`)
+}
---
+++ aula-11/ex009-p.js
@@ -0,0 +1,7 @@
+var nota = 3
+console.log (`Você tirou ${nota} pontos,`)
+if (nota >= 6){
+    console.log('E foi aprovado!!!')
+} else{
+    console.log('E foi reprovado :d')
+}
