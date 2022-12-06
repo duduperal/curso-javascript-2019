ad604cdac757bf030c1bb6984cd80167516f667e
José Eduardo Goulart Peral <109534745+duduperal@users.noreply.github.com>
2022-12-05 22:37:45 -0300

    aula-12

---
+++ aula-12/ex011-p.js
@@ -0,0 +1,11 @@
+var nota = 10
+console.log (`Você tirou ${nota} pontos!`)
+if (nota < 3){
+    console.log(`Você tirou muito a baixo da média tome cuidado!`)
+} else if (nota < 6){
+    console.log(`Você tirou vermelho estude um pouco mais que você irá conseguir.`)
+} else if (nota <= 9){
+    console.log(`Você tirou azul parabéns :D`)
+} else {
+    console.log(`Você FECHOU a prova! Parabéns você será uma pessoa muito inteligente.`)
+}
