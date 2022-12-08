b755e8597275f753757da78062b39552628c61c8
José Eduardo Goulart Peral <109534745+duduperal@users.noreply.github.com>
2022-12-08 12:50:43 -0300

    aula-13

---
+++ aula-13/ambiente.js
@@ -0,0 +1,28 @@
+//REPETIÇÃO COM TESTE LÓGICO NO FINAL!
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+//------------------------------------
+
+/*
+REPETIÇÃO COM TESTE LÓGICO NO COMEÇO!
+
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/ 
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
