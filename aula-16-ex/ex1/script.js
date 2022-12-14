f7b0e6b0e9352c8ba7c07a7e34226aca42198c43
José Eduardo Goulart Peral <109534745+duduperal@users.noreply.github.com>
2022-12-14 12:29:16 -0300

    ex018

--- aula-16-ex/ex1/script.js
+++
@@ -1,8 +0,0 @@
-var n = [document.getElementById('txtn')]
-var res = document.getElementById('res')
-
-function adicionar(){
-    for(let c in n){
-        res.innerHTML = `Valor ${n[c]} adicionado`
-    }
-}
